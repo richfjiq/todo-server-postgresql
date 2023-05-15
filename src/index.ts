cf70205395db2ffea309251ba153ac57db956e87
richfjiq <rfjiq1986@gmail.com>
2023-05-14 19:32:49 -0600

    chore: add signup route

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { v4 as uuidv4 } from 'uuid';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 import { pool } from './db';
 
@@ -73,6 +75,37 @@ app.delete('/todos/:id', async (req: Request, res: Response) => {
       id,
     ]);
     res.status(200).json(deleteTodo);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.post('/todos/signup', async (req, res) => {
+  const { email, password } = req.body;
+  const salt = bcrypt.genSaltSync(10);
+  const hashedPassword = bcrypt.hashSync(password, salt);
+
+  try {
+    const signup = await pool.query(
+      `INSERT INTO users (email, hashed_password) VALUES($1, $2)`,
+      [email, hashedPassword]
+    );
+
+    const token = jwt.sign({ email }, process.env.SECRET_JWT ?? '', {
+      expiresIn: '1d',
+    });
+
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(403).json({ message: 'Email already exists.' });
+    console.error(error);
+  }
+});
+
+app.post('/todos/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
   } catch (error) {
     console.error(error);
   }
